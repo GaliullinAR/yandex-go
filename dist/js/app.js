1e45476c00d1dc6b52bd9783e77d5d5edc91c352
Galiullin Azat <galiullin.ar651@gmail.com>
2022-09-16 23:06:03 +0300

    Сверстал десктоп

---
+++ dist/js/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(function () {
+  $('.slider').slick({
+    slideToShow: 1,
+    slideToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
+});
