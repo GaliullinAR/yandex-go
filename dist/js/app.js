63f4ea7045784e8e545c94e877c240c01c718608
Galiullin Azat <galiullin.ar651@gmail.com>
2022-09-17 19:04:59 +0300

    Частично выполнил адаптивную верстку

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,11 +1,27 @@
 "use strict";
 
 $(function () {
-  $('.slider').slick({
+  $(".slider").slick({
     slideToShow: 1,
     slideToScroll: 1,
     arrows: false,
     dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
+  $('.tarification__slider').slick({
+    dots: true,
+    slideToShow: 1,
+    slideToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 5000,
+    arrows: false
+  });
+  $('.abonement__slider-info').slick({
+    arrows: false,
+    dots: true,
+    slideToShow: 1,
+    sliderToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000
   });
